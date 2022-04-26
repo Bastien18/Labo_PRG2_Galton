1133e877605621da0bc99347caff13762d586ab8
Pillonel-on-github <bastien.pil01@gmail.com>
2022-04-26 11:14:17 +0200

    Canvas de base

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : <nom du fichier>.<x> (x = h ou c)
+ Auteur(s)      : <pr�nom> <nom>
+ Date creation  : <jj.mm.aaaa>
 
-int main() {
-   printf("Hello, World!\n");
-   return 0;
+ Description    : <� compl�ter>
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0 
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+   <code>
+   return EXIT_SUCCESS;
 }
