040fc912d27243cb919dee916656d1fde494bfbd
Pillonel-on-github <bastien.pil01@gmail.com>
2022-04-26 22:14:12 +0200

    Les billes elles tombent mdrrr

--- main.c
+++ main.c
@@ -16,13 +16,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <math.h>
 
 //TODO Rechecker les types de données
 
 void Galton(unsigned nbrDeBilles, unsigned nbrDeClou, unsigned* tableauCompteur,
             unsigned* tableauBacBilles);
 
+void afficherTab(const unsigned* tab, size_t taille);
+
 int main(void) {
+
+   unsigned nbrDeClou = 10,
+            nbrDeBilles = 1000;
+
+   const size_t TAILLE_TABLEAU_COMPTEUR   = nbrDeClou * (nbrDeClou - 1) / 2;
+   const size_t TAILLE_TABLEAU_BAC_BILLES = nbrDeClou + 1;
+
+   unsigned* tableauCompteur  = (unsigned*) calloc(TAILLE_TABLEAU_COMPTEUR,
+                                         sizeof(unsigned));
+   unsigned* tableauBacBilles = (unsigned*) calloc(TAILLE_TABLEAU_BAC_BILLES,
+                                         sizeof(unsigned));
+
+   Galton(nbrDeBilles,nbrDeClou,tableauCompteur, tableauBacBilles);
+
+   afficherTab(tableauBacBilles, nbrDeClou + 1);
+
+   free(tableauBacBilles);
+   free(tableauCompteur);
+
    return EXIT_SUCCESS;
 }
 
@@ -36,13 +58,27 @@ void Galton(unsigned nbrDeBilles, unsigned nbrDeClou, unsigned* tableauCompteur,
       srand(time(NULL));
    }
 
-   const size_t TAILLE_TABLEAU_COMPTEUR   = nbrDeClou * (nbrDeClou - 1) / 2;
-   const size_t TAILLE_TABLEAU_BAC_BILLES = nbrDeClou + 1;
+   // Boucle faisant tomber les billes
+   for(unsigned i = 0u; i < nbrDeBilles; ++i){
 
+      // Boucle faisant tomber une bille
+      size_t emplacementBille = 0;
+      for(unsigned j = 0u; j < nbrDeClou; ++j){
+         emplacementBille += (size_t) (rand() % 2);
+      }
 
+      // Sécurité en cas d'overflow on arrête d'augmenter le nbr de bille
+      if(tableauBacBilles[emplacementBille] < UINT_MAX)
+         ++tableauBacBilles[emplacementBille];
+   }
+}
 
-   tableauCompteur  = (unsigned*) calloc(TAILLE_TABLEAU_COMPTEUR,
-                                                  sizeof(unsigned ));
-   tableauBacBilles = (unsigned*) calloc(TAILLE_TABLEAU_BAC_BILLES,
-                                                   sizeof(unsigned ));
+void afficherTab(const unsigned* tab, size_t taille){
+   printf("[");
+   for(size_t i = 0; i < taille; ++i){
+      if(i > 0)
+         printf(",");
+      printf("%u", tab[i]);
+   }
+   printf("]");
 }
