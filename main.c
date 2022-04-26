b07cef4ba4608e7bb13ba81186d9dd3964723609
Pillonel-on-github <bastien.pil01@gmail.com>
2022-04-26 12:03:31 +0200

    Galton func

--- main.c
+++ main.c
@@ -14,7 +14,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+//TODO Rechecker les types de données
+
+void Galton(unsigned nbrDeBilles, unsigned nbrDeClou, unsigned* tableauCompteur,
+            unsigned* tableauBacBilles);
 
 int main(void) {
    return EXIT_SUCCESS;
+}
+
+void Galton(unsigned nbrDeBilles, unsigned nbrDeClou, unsigned* tableauCompteur,
+            unsigned* tableauBacBilles){
+
+   static bool estRandomInitialise = false;
+
+   if(!estRandomInitialise){
+      estRandomInitialise = true;
+      srand(time(NULL));
+   }
+
+   const size_t TAILLE_TABLEAU_COMPTEUR   = nbrDeClou * (nbrDeClou - 1) / 2;
+   const size_t TAILLE_TABLEAU_BAC_BILLES = nbrDeClou + 1;
+
+
+
+   tableauCompteur  = (unsigned*) calloc(TAILLE_TABLEAU_COMPTEUR,
+                                                  sizeof(unsigned ));
+   tableauBacBilles = (unsigned*) calloc(TAILLE_TABLEAU_BAC_BILLES,
+                                                   sizeof(unsigned ));
 }
