6c061f58325935047d9cbed0dfbff8619bda0686
Brasey-on-github <35539296+Brasey-on-github@users.noreply.github.com>
2022-04-28 16:15:39 +0200

    hey

--- GaltonBoard.c
+++ GaltonBoard.c
@@ -22,100 +22,160 @@
  -----------------------------------------------------------------------------------
 */
 
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <math.h>
 #include <time.h>
 
-// Constantes
-#define MAX_BILLES 30000
-#define MIN_BILLES 1000
-#define MAX_RANGEE 20
-#define MIN_RANGEE 10
-#define MAX_SAISIE 6
-#define MAX_DISPLAY 5
-#define TAILLE_HIST 15
+// Define de constantes
+#define MAX_DIGIT_SAISIE 6
 #define ERROR_MSG "Saisie incorrecte. Veuillez SVP recommencer.\n"
+#define BILLES_MSG "Entrez le nombre de billes"
+#define RANGEE_MSG "Entrez le nombre de rangees de compteurs"
 
-// Macro pour convertir la valeur d'un define est chaine de caractère.
+// Macro pour convertir la valeur d'un define en une chaine de caractère.
 #define _STR(x) #x
 #define STR(x) _STR(x)
 
-// Fonction pour vider le buffer après une saisie.
-void vider_Buffer(){
-	scanf("%*[^\n]");
-	getchar();
+// Macro pour définir une fonction de saisie pour un type donnée.
+// - la fonction vien afficher la chaine de caractère du parametre question.
+// - Puis l'intervale admissible de fini par min et max,
+// - Ensuite l'utilisateur va devoir faire une saisie
+// - En cas de saisie incorrecte ou hors de l'intervale,
+//    la fonction affiche error_msg et recommance depuis le début
+#define SAISIE_INTERVALE(type,format_print, format_scan,nom)                     \
+void saisie_Interval_##nom (type * retour, type min, type max, char* question,   \
+									 char* error_msg){                                    \
+	assert(min < max);                                                            \
+	do{                                                                           \
+      printf("%s [%" format_print " - %" format_print "] : ",question,min, max); \
+	}while((!(scanf("%" format_scan "%*[^\n]",retour)) ||                         \
+				*retour > max || *retour < min) &&                                   \
+				(scanf("%*[^\n]"),printf( error_msg),1));                            \
+}                                                                                \
+
+// Alias pour le type principal du programme
+#define FORMAT_COMPTEUR PRIu32
+typedef uint32_t  type_compteur;
+
+// Fonction de saisie via la macro
+SAISIE_INTERVALE(type_compteur , FORMAT_COMPTEUR, STR(MAX_DIGIT_SAISIE) FORMAT_COMPTEUR,
+                 billes)
+SAISIE_INTERVALE(size_t ,"zu", STR(MAX_DIGIT_SAISIE) "zu", size_t)
+
+// Fonction qui prend un nombre de type type_compteur et retourne le nombre de
+// digits de ce nombre.
+size_t nbr_digit(type_compteur n){
+	size_t nbr = 0;
+	for(;n>0;nbr++)n/=10;
+	return nbr;
+}
+
+// Fonction qui génère une planche de Galton de type type_compteur et affiche une
+// représentation si désiré
+type_compteur* genererPlancheDeGalton(type_compteur nbr_billes,size_t nbr_rangee,
+												  bool imprimer){
+	assert(nbr_rangee);
+	static bool deja_init = false;
+	type_compteur*  tab_clous  = calloc(nbr_rangee*(nbr_rangee+1)/2,
+	                                                     sizeof(type_compteur));
+	if(tab_clous){
+		
+		// Init.
+		*tab_clous = nbr_billes;
+		type_compteur* ptr_rangee = tab_clous;
+		size_t nbr_digits = 0;
+		size_t decalage  = 0;
+		if(!deja_init){
+			srand(time(NULL));
+			deja_init = true;
+		}
+		
+		// Init de variable d'affichage
+		if(imprimer){
+			nbr_digits = nbr_digit(nbr_billes);
+			nbr_digits |= 1; // pour que un nombre de digit pair s'affiche correctement
+			decalage = (nbr_digits+ 1)/2;
+		}
+		
+		// Simulation et affichage de la planche de Galton
+		for(size_t n_rangee = 0ull; n_rangee < nbr_rangee; ++n_rangee){
+			// Mettre le pointeur au début de la ligne
+			ptr_rangee += n_rangee;
+			for(size_t n_clou = 0ull ; n_clou <= n_rangee; n_clou++) {
+				// Determiner la destination des billes si besoin
+				if (n_rangee < nbr_rangee - 1) {
+					for (type_compteur bille = 0; bille < ptr_rangee[n_clou]; ++bille) {
+						ptr_rangee[n_rangee + 1 + n_clou + rand() % 2 ]++;
+					}
+				}
+				// Afficher le clou actuel
+				if(imprimer)printf("%*" FORMAT_COMPTEUR " " ,nbr_digits + (n_clou?0:(
+					(nbr_rangee-n_rangee-1)*decalage)), ptr_rangee[n_clou]);
+			}
+			if(imprimer)printf("\n");
+		}
+	}
+	return tab_clous;
+}
+
+// Fonction qui prend un tableau en paramètre et qui génére un histogramme
+// des valeurs du tableau.
+void histogramme(const type_compteur*  tab, size_t nbr_elements, size_t taille,
+					  size_t espacement){
+	
+	// Trouver la valeur max du tableau
+	type_compteur max = 0;
+	for(size_t i = 0;i < nbr_elements; i++){
+		if(tab[i] > max) max = tab[i];
+	}
+	
+	// Affichage de l'histogramme
+	espacement |= 1; // pour qu'un nombre de digit pair s'affiche correctement
+	for(size_t etage = taille; etage; etage--){
+		for(size_t i = 0;i < nbr_elements; i++)
+			printf("%*c ",espacement ,(round(tab[i] / (max /(double) taille)) >=
+			(double)(etage)) ? '*' : ' ');
+		printf("\n");
+	}
 }
 
 int main(void) {
 	
 	// Initialisation et Constante
-	const uint8_t DECALAGE = (MAX_DISPLAY + 1)/2;
-	uint32_t nbr_billes, nbr_rangee;
+	const type_compteur  MAX_BILLES  = 30000,
+								MIN_BILLES  = 1000;
+	const size_t         MAX_RANGEE  = 20,
+								MIN_RANGEE  = 10,
+								TAILLE_HIST = 15;
+	type_compteur        nbr_billes;
+	size_t				   nbr_rangee;
 	
 	// Saisie du nombre de billes
-	do{
-		printf("Entrez le nombre de billes [%d - %d] : ",MIN_BILLES, MAX_BILLES);
-		
-		// Verifie que la saisie est correcte et réagit en cas d'erreur
-	}while((!(scanf("%" STR(MAX_SAISIE) "u%*[^\n]",&nbr_billes)) ||
-		   nbr_billes > MAX_BILLES || nbr_billes < MIN_BILLES) &&
-			(vider_Buffer(),printf( ERROR_MSG),1));
+	saisie_Interval_billes(&nbr_billes , MIN_BILLES, MAX_BILLES, BILLES_MSG,
+								  ERROR_MSG);
 	
 	// Saisie du nombre de rangées
-	do{
-		printf("Entrez le nombre de rangees de compteurs [%d - %d]",MIN_RANGEE,
-				 MAX_RANGEE);
-		// Verifie que la saisie est correcte et réagit en cas d'erreur
-	}while((!(scanf("%" STR(MAX_SAISIE)"u%*[^\n]",&nbr_rangee)) ||
-	        nbr_rangee > MAX_RANGEE || nbr_rangee < MIN_RANGEE) &&
-	       (vider_Buffer(),printf( ERROR_MSG),1));
+	saisie_Interval_size_t(&nbr_rangee, MIN_RANGEE, MAX_RANGEE, RANGEE_MSG,
+								  ERROR_MSG);
 	
-	
-	// allocation de notre planche de Galton
-	uint32_t* const tab_clous  = (uint32_t* const)calloc(nbr_rangee*(nbr_rangee+1)/2,
-	                                                sizeof(uint32_t));
-	
-	// Préparation pour la simulation
-	*tab_clous = nbr_billes;
-	uint32_t* ptr_rangee = tab_clous;
-	srand(time(NULL));
+	// Génération et impression de la planche de Galton
 	printf("\n");
+	type_compteur* galtonBoard = genererPlancheDeGalton(nbr_billes,nbr_rangee,true);
+	if(!galtonBoard)return EXIT_FAILURE;
 	
-	// Simulation et affichage de la planche de Galton
-	for(unsigned n_rangee = 0u; n_rangee < nbr_rangee; ++n_rangee){
-		// Mettre le pointeur au début de la ligne
-		ptr_rangee += n_rangee;
-		for(unsigned n_clou = 0 ; n_clou <= n_rangee; n_clou++) {
-			// Determiner la destination des billes si besoin
-			if (n_rangee < nbr_rangee - 1) {
-				for (unsigned bille = 0u; bille < ptr_rangee[n_clou]; ++bille) {
-					ptr_rangee[n_rangee + 1 + n_clou + rand() % 2]++;
-				}
-			}
-			// Afficher le clou actuel
-			printf("%*d ",MAX_DISPLAY + (n_clou?0:((nbr_rangee- n_rangee-1)*
-			DECALAGE)), ptr_rangee[n_clou]);
-		}
-		printf("\n");
-	}
 	
-	// Trouver la valeur max de la dernière rangée
-	uint32_t max = 0;
-	for(int i = 0;i < nbr_rangee; i++){
-		if(ptr_rangee[i] > max) max = ptr_rangee[i];
-	}
-	
-	// Affichage de l'histogramme
+	// Affichage de l'histogramme de la dernière ligne
 	printf("\n");
-	for(uint32_t etage = TAILLE_HIST; etage; etage--){
-		for(int i = 0;i < nbr_rangee; i++) printf("%*c ",MAX_DISPLAY ,
-		(round(*(ptr_rangee + i) / ((double)max / TAILLE_HIST)) >= (etage)) ? '*' : ' ');
-		printf("\n");
-	}
+	histogramme(galtonBoard +(nbr_rangee -1)*nbr_rangee/2,nbr_rangee,TAILLE_HIST,
+	            nbr_digit(*galtonBoard));
 	
 	// Libération de la mémoire et fin de programme
-	free(tab_clous);
+	free(galtonBoard);
    return EXIT_SUCCESS;
 }
